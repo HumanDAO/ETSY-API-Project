b015e3854942d10c518e1f2175740ae6ea10f72c
robdesousa <eee.roberto@gmail.com>
2014-07-16 18:08:14 -0500

    version1

--- js/code.js
+++ js/code.js
@@ -1,4 +1,28 @@
-<script>
+$.(function() {
+
+            function EtsyClient(option) {
+                this.etsy_url = "https://openapi.etsy.com/";
+                this.api_version = option.api_version || "v2/";
+                this.model = "listings/";
+                this.filter = "active";
+                this.js = ".js";
+                this.api_key = "2s69xib2039fs6asqnkosxfo"; //Roberto's key
+                this.complete_api_url = this.etsy_url + this.api_version;
+
+            }
+
+            APIMAIN.prototype.getRequest() {
+                $.getJSON(complete_api_url + this.model + this.filter + 
+                	this.js + "?" + "api_key=" + api_key + "&callback=?").
+                then(function(data) {
+                        console.log(data);
+                    });
+                }
+                    // Etsy_Template = ._template(list, Object);
+                    //$.(".)
+            
+                
+});
 
 
-</script>
+        
